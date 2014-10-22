cff27f70fc97e062c0aef933761aa3fb832260e1
genghis88 <kumar.bharath1988@gmail.com>
2014-10-22 07:59:18 -0400

    Local search fixed

--- src/solver/GLSSolver.java
+++ src/solver/GLSSolver.java
@@ -17,8 +17,8 @@ public class GLSSolver implements ISolver {
     population.add(a);
 
     int t = 0;
-    population.set(t, localSearch(population.get(t)));
-    population.add(population.get(0));
+    population.add(localSearch(population.get(t), f));
+    // population.add(population.get(0));
     int num = f.numClausesSatisfied(population.get(t));
     while (num < (f.clauseCount())) {
       t++;
@@ -31,7 +31,7 @@ public class GLSSolver implements ISolver {
         for (int j = f.maxLiteral() / 2; j < f.maxLiteral(); j++) {
           b.values[j] = population.get(t).values[j];
         }
-        b = localSearch(b);
+        b = localSearch(b, f);
         population.add(b);
       }
     }
@@ -39,19 +39,20 @@ public class GLSSolver implements ISolver {
     return population.get(population.size() - 1);
   }
 
-  public Assignment localSearch(Assignment a) {
+  public Assignment localSearch(Assignment a, Formula f) {
     int improve = 1;
     while (improve > 0) {
       improve = 0;
       int i = 1;
       while (i < a.values.length - 1) {
+        int num = f.numClausesSatisfied(a);
         a.values[i] = !a.values[i];
-        // calculate gain
-        int gain = 0;
+        int gain = f.numClausesSatisfied(a) - num;
         if (gain >= 0) {
           // do nothing (keep flip)
           improve += gain;
         } else {
+          // flip back
           a.values[i] = !a.values[i];
         }
         i++;
